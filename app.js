13b59aa61084f3c075654da5ee068b3971e43c6f
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-08-26 14:23:18 -0700

    created calcul8er.. plus, minus and times

--- app.js
+++ app.js
@@ -1,9 +1,55 @@
 // import functions
 
+import { add } from "./calculator.js";
+import { subtract } from "./calculator.js";
+import { multiply } from "./calculator.js";
+
+
 // reference needed DOM elements
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
+ 
 
 
 // set event listeners 
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+addButton.addEventListener('click', () => {
+     // get user input(s)
+    const num1 = Number(addNumber1.value);
+    const num2 = Number(addNumber2.value);
+        // do any needed work with the value(s)
+    const sum = add(num1, num2);
+     // update DOM to reflect new value(s)
+    addAnswer.textContent = sum;
+       
+});
+
+const subtractNumber1 = document.getElementById('subtract-number-1');
+const subtractNumber2 = document.getElementById('subtract-number-2');
+const subtractButton = document.getElementById('subtract-button');
+const subtractAnswer = document.getElementById('subtract-answer');
+ 
+subtractButton.addEventListener('click', () => {
+    const num1 = Number(subtractNumber1.value);
+    const num2 = Number(subtractNumber2.value);
+        // do any needed work with the value(s)
+    const difference = subtract(num1, num2);
+
+    subtractAnswer.textContent = difference;
+
+}); 
+
+const multiplyNumber1 = document.getElementById('multiply-number-1');
+const multiplyNumber2 = document.getElementById('multiply-number-2');
+const multiplyButton = document.getElementById('multiply-button');
+const multiplyAnswer = document.getElementById('multiply-answer');
+
+multiplyButton.addEventListener('click', () => {
+    const num1 = Number(multiplyNumber1.value);
+    const num2 = Number(multiplyNumber2.value);
+
+    const product = multiply(num1, num2);
+
+    multiplyAnswer.textContent = product;
+});
--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,48 @@
 // IMPORT MODULES under test here:
 // import { add } from '../calculator.js';
+import { add } from '../calculator.js';
+
+import { subtract } from '../calculator.js';
+
+import { multiply } from '../calculator.js';
 
 const test = QUnit.test;
 
+
+
 // name your test by what it is testing
-test('time to test a function', (expect) => {
+test('add two numbers', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 1;
+    const y = 3;
+    const expected = 4;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = add(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('subtract two numbers', (expect) => {
+    const x = 6;
+    const y = 3;
+    const expected = 3;
+
+    const actual = subtract(x, y);
+    expect.equal(actual, expected);
+
+});
+
+test('multiply two numbers', (expect) => {
+    const x = 5;
+    const y = 10;
+    const expected = 50;
+
+    const actual = multiply(x, y);
+    expect.equal(actual, expected);
+
 });
